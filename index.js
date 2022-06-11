80c3335b46e701c39e21d62e0c4630edaecba461
Emmanuel Ovares <eovares@cisco.com>
2022-06-11 07:24:18 -0600

    Routes creation

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const faker = require('faker');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, nueva ruta');
+})
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products);
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter')
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+      id,
+      name: 'Producto 1',
+      price: 1000
+  });
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros');
+  }
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+      categoryId,
+      productId
+  });
+})
+
+app.get('/categories', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+})
+app.get('/home', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+})
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
+
